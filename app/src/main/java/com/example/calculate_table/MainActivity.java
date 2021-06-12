204154e309d96f5e186443531bfb822f569b54c6
abhijitmondal121 <mondalavi9836@gmail.com>
2021-06-13 00:23:08 +0530

    hello_world

---
+++ app/src/main/java/com/example/calculate_table/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.calculate_table;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    ListView listView;
+    SeekBar seekbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.textView);
+        listView=findViewById(R.id.listview);
+        seekbar=findViewById(R.id.seekBar);
+        seekbar.setMax(20);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Toast.makeText(MainActivity.this, "The populating table of "+ progress, Toast.LENGTH_SHORT).show();
+                populate(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+    public  void populate(int table){
+        ArrayList<String>mulTable=new ArrayList<>();
+        for(int i=1;i<=10;i++){
+            mulTable.add(table+" X "+i+" = "+table*i);
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,mulTable);
+        listView.setAdapter(arrayAdapter);
+    }
+}
